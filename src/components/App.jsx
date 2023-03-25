49fc6fc6b93e67eda86a98a349c07af8738b00d1
Sonu Ram <sonuram6960@gmail.com>
2023-03-25 20:28:39 +0530

    Run effect on update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const App = () => {
-  return <div>Hello World!</div>;
+  const isMounted = useRef();
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    if (isMounted.current) {
+      console.log("Updated!");
+    } else {
+      console.log("Mounted!");
+      isMounted.current = true;
+    }
+  });
+
+  return (
+    <div>
+      <h1>You clicked {count} times</h1>
+      <button onClick={() => setCount((count) => count + 1)}>Click</button>
+    </div>
+  );
 };
 
 export default App;
